d8ad827923bbdfcb6fed2a2b808bb6db95ed3338
TruX87 <163743905+TruX87@users.noreply.github.com>
2024-03-22 16:44:03 +0200

    javascript kodus

---
+++ 2javascript.js
@@ -0,0 +1,50 @@
+console.log("Töötab")
+
+nimi = "Kalle"
+If (nimi.length > 1); {
+    console.log("Nimi on korrektne")
+} else {
+    console.log("Nimi on liiga lühike")
+}
+
+nimi = "K"
+If (nimi.length > 1); {
+    console.log("Nimi on korrektne")
+} else {
+    console.log("Nimi on liiga lühike")
+}
+    
+vanus = 25
+If (vanus >= 18); {
+    console.log("Isik on täisealine")
+} else {
+    console.log("Isik on alaealine")
+}
+
+vanus = 17
+If (vanus >= 18); {
+    console.log("Isik on täisealine")
+} else {
+    console.log("Isik on alaealine")
+}
+
+email = "kalle25@gmail.com"
+if (email.includes("@")) {
+    console.log("Email on korrektne")
+} else {
+    console.log("Email ei ole korrektne")
+}
+    
+registreerunud = true
+if (registreerunud) {
+    console.log("Registreerunud")
+} else {
+    console.log("Mitte registreerunud")
+}
+
+isikukood = "00000000001"
+If (isikukood.length >= 11); {
+    console.log("Email on korrektne")
+} else {
+    console.log("Email pole korrektne")
+}
