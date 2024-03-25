14fc1af5c72338814cfb827b90c7b987d5ff8b5a
TruX87 <163743905+TruX87@users.noreply.github.com>
2024-03-25 22:07:12 +0200

    javascript kodus

--- 2javascript.js
+++ 2javascript.js
@@ -1,51 +1,83 @@
 console.log("Töötab")
 
 nimi = "Kalle"
-If (nimi.length > 1); {
-    console.log("Nimi on korrektne")
-} else {
-    console.log("Nimi on liiga lühike")
+console.log(nimi)
+    if (nimi.length > 1) {
+        console.log("Nimi on korrektne")
+    } else {
+        console.log("Nimi on liiga lühike")
 }
 
 nimi = "K"
-If (nimi.length > 1); {
-    console.log("Nimi on korrektne")
-} else {
-    console.log("Nimi on liiga lühike")
+console.log(nimi)
+    if (nimi.length > 1) {
+        console.log("Nimi on korrektne")
+    } else {
+        console.log("Nimi on liiga lühike")
 }
     
 vanus = 25
-vanus = true
-If (vanus >= 18); {
-    console.log("Isik on täisealine")
-} else {
-    console.log("Isik on alaealine")
+console.log(vanus)
+    if (vanus >= 18) {
+        console.log("Isik on täisealine")
+    } else {
+        console.log("Isik on alaealine")
 }
 
 vanus = 17
-If (vanus >= 18); {
-    console.log("Isik on täisealine")
-} else {
-    console.log("Isik on alaealine")
+console.log(vanus)
+    if (vanus >= 18) {
+        console.log("Isik on täisealine")
+    } else {
+        console.log("Isik on alaealine")
 }
 
 email = "kalle25@gmail.com"
-if (email.includes("@")) {
-    console.log("Email on korrektne")
-} else {
-    console.log("Email ei ole korrektne")
-}
-    
-registreerunud = true
-if (registreerunud) {
-    console.log("Registreerunud")
-} else {
-    console.log("Mitte registreerunud")
+console.log(email)
+    if (email.includes("@")) {
+        console.log("Email on korrektne")
+    } else {
+        console.log("Email ei ole korrektne")
 }
 
+email = "kalle25.gmail.com"
+console.log(email)
+    if (email.includes("@")) {
+        console.log("Email on korrektne")
+    } else {
+        console.log("Email ei ole korrektne")
+}
+
+registreerunud = true
+    if (registreerunud) {
+        console.log("Registreerunud")
+    } else {
+        console.log("Mitte registreerunud")
+}
+
+const str1 = 'E-mail saadetud';
+const str2 = 'sellele aadressile';
+
+console.log(str1.concat(' ', str2));
+    console.log(email)
+
+const str3 = 'E-maili ei';
+const str4 = 'saadetud';    
+
+console.log(str3.concat(' ', str4));
+
 isikukood = "00000000001"
-If (isikukood.length >= 11); {
-    console.log("Isikukood on korrektne")
-} else {
-    console.log("Eisikukood ei ole korrektne")
+console.log(isikukood)
+    if (isikukood.length >= 11) {
+        console.log("Isikukood on korrektne")
+    } else {
+        console.log("Eisikukood ei ole korrektne")
+}
+
+isikukood = "0000000000"
+console.log(isikukood)
+    if (isikukood.length >= 11) {
+        console.log("Isikukood on korrektne")
+    } else {
+        console.log("Eisikukood ei ole korrektne")
 }
