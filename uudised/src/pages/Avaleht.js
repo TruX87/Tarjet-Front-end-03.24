834917ffa7c3bbd92290bbc20d686635fedb879e
TruX87 <163743905+TruX87@users.noreply.github.com>
2024-03-31 21:52:11 +0300

    uudised-osa2-kodus

--- uudised/src/pages/Avaleht.js
+++ uudised/src/pages/Avaleht.js
@@ -4,7 +4,7 @@ function Avaleht() {
   return (
     <div>
        <div>Avaleht, siin olev info on nähtav localhost:3000 aadressil</div> 
-       <img src="https://st2.depositphotos.com/6789684/12262/v/450/depositphotos_122620866-stock-illustration-illustration-of-flat-icon.jpg" alt="" />
+       <img className="Header-logo" src="https://st2.depositphotos.com/6789684/12262/v/450/depositphotos_122620866-stock-illustration-illustration-of-flat-icon.jpg" alt="" />
     </div>
   )
 }
