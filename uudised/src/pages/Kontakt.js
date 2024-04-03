ca7c421a2b543d9d0a87d71e24abe03f3e5733e0
TruX87 <163743905+TruX87@users.noreply.github.com>
2024-04-03 22:06:29 +0300

    uudiste-j2tk

--- uudised/src/pages/Kontakt.js
+++ uudised/src/pages/Kontakt.js
@@ -1,22 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Kontakt() {
+  const [n2itaTelKristiine, muudaN2itaTelKristiine] = useState(false)
+  const [n2itaTelYlemiste, muudaN2itaTelYlemiste] = useState(false)
+  const [n2itaTelTasku, muudaN2itaTelTasku] = useState(false)
+
   return (
     <div>
         <div>Kontakt, siin olev info on nähtav localhost:3000/kontakt aadressil</div>
         <div>Võta meiega ühendust:</div>
         <br />
-        <div>Kristiine keskus</div>
-        <div>+3721231231</div>
-        <div>Endla 45</div>
+        <div className={n2itaTelKristiine === true ? "valitud" : undefined} onClick={() => muudaN2itaTelKristiine(!n2itaTelKristiine)}>Kristiine keskus</div>
+        {n2itaTelKristiine && <div className="valitud">+3721231231</div>}
+        <div className={n2itaTelKristiine === true ? "valitud" : undefined}>Endla 45</div>
         <br />
-        <div>Ülemiste keskus:</div>
-        <div>+3723213213</div>
-        <div>Suur-Sõjamäe 4</div>
+        <div className={n2itaTelYlemiste === true ? "valitud" : undefined} onClick={() => muudaN2itaTelYlemiste(!n2itaTelYlemiste)}>Ülemiste keskus:</div>
+        {n2itaTelYlemiste && <div className="valitud">+3723213213</div>}
+        <div className={n2itaTelYlemiste === true ? "valitud" : undefined}>Suur-Sõjamäe 4</div>
         <br />
-        <div>Tasku keskus:</div>
-        <div>+3722132132</div>
-        <div>Turu 2</div>
+        <div className={n2itaTelTasku === true ? "valitud" : undefined} onClick={() => muudaN2itaTelTasku(!n2itaTelTasku)}>Tasku keskus:</div>
+        {n2itaTelTasku && <div className="valitud">+3722132132</div>}
+        <div className={n2itaTelTasku === true ? "valitud" : undefined}>Turu 2</div>
     </div>
   )
 }
--- uudised/src/pages/Meist.js
+++ uudised/src/pages/Meist.js
@@ -1,25 +1,30 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Meist() {
+  const [kontakt, n2itaKontakt] = useState("")
   return (
     <div>
         <div>Meist, siin olev info on nähtav localhost:3000/meist aadressil</div>
         <div>Meie töötajad:</div>
         <br />
         <div>Arvo Pärt</div>
-        <div>+3727897897</div>
         <div>Uudisklippide taustamuusika</div>
-        <br />
+        <button onClick={() => n2itaKontakt("+3727897897")}>Võta ühendust</button>
+        <br /><br />
         <div>Kelly Sildaru</div>
-        <div>+3724564564</div>
         <div>Püstolreporter</div>
-        <br />
+        <button onClick={() => n2itaKontakt("+3724564564")}>Võta ühendust</button>
+        <br /><br />
         <div>Edward von Lõngus</div>
-        <div>+3726546546</div>
-        <br />
+        <div>Uudiste kujundamine</div>
+        <button onClick={() => n2itaKontakt("+3726546546")}>Võta ühendust</button>
+        <br /><br />
         <div>Kerli Kõiv</div>
-        <div>+3729879879</div>
         <div>Välisturgude spetsialist</div>
+        <button onClick={() => n2itaKontakt("+3729879879")}>Võta ühendust</button>
+        <br /><br />
+        { kontakt !== "" && <div>Tema kontakt: {kontakt}</div>}
     </div>
   )
 }
