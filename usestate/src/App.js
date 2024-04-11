3a1884f9ad7535cf995bf3397074b56c1bc45e89
TruX87 <163743905+TruX87@users.noreply.github.com>
2024-04-11 11:24:37 +0300

    usestate-telia-kodus

--- usestate/src/App.js
+++ usestate/src/App.js
@@ -3,6 +3,8 @@ import {Link, Navigate, Route, Routes} from 'react-router-dom'
 import Ilmumine from './pages/Ilmumine';
 import Kujundus from './pages/Kujundus';
 import Muutmine from './pages/Muutmine';
+import Telefon from './pages/Telefon';
+import Hind from './pages/Hind';
 
 function App() {
   return (
@@ -11,12 +13,16 @@ function App() {
       <Link to="/ilmumine">Ilmumine</Link>
       <Link to="/kujundus">Kujundus</Link>
       <Link to="/muutmine">Muutmine</Link>
+      <Link to="/telefon">Telefon</Link>
+      <Link to="/hind">Hind</Link>
       
       <Routes>
         <Route path="/" element={<Navigate to="/ilmumine" />} />
         <Route path="/ilmumine" element={<Ilmumine />} />
         <Route path="/kujundus" element={<Kujundus />} />
         <Route path="/muutmine" element={<Muutmine />} />
+        <Route path="/telefon" element={<Telefon />} />
+        <Route path="/hind" element={<Hind />} />
       </Routes>
     </div>
   );
