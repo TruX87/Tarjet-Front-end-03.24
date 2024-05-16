7eb753a2f43185e72bd86b091e825417deddd40b
TruX87 <163743905+TruX87@users.noreply.github.com>
2024-05-16 12:22:09 +0300

    webshop-API- kogus ostukorvis

--- webshop/src/pages/global/HomePage.jsx
+++ webshop/src/pages/global/HomePage.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 
 
 function HomePage() {
-  const [products, setProducts] = useState(productsFromFile);
+  const [products] = useState(productsFromFile);
   
 
 function addToCart(product) {
