5b21be5bb5bca114acf75cb64d03550d7e312492
TruX87 <163743905+TruX87@users.noreply.github.com>
2024-05-26 19:32:31 +0300

    webshop-idunique-api

--- webshop/src/components/Map.jsx
+++ webshop/src/components/Map.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
@@ -8,42 +9,86 @@ import '../css/Map.css';
 let DefaultIcon = L.icon({
   iconUrl: icon,
   shadowUrl: iconShadow,
-  iconSize: [25,41], 
-  iconAnchor: [12,41],
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
   popupAnchor: [2, -40],
 });
 L.Marker.prototype.options.icon = DefaultIcon;
 
-function Map(props) { 
+function Map({ mapCoordinates }) {
+  const defaultPosition = [58.7771, 24.9038]; // Default center position
 
   return (
-  <div>
-
-    <MapContainer className='map' center={props.mapCoordinaates.lngLat} zoom={props.mapCoordinaates.zoom} scrollWheelZoom={true}>
-      <ChangeView center={props.mapCoordinaates.lngLat} zoom={props.mapCoordinaates.zoom} />
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[59.4231, 24.7991]}>
-        <Popup>
-          Ülemiste keskus. <br /> Avatud 9-20
-        </Popup>
-      </Marker>
-      <Marker position={[59.4277, 24.7193]}>
-        <Popup>
-          Kristiine keskus. <br /> Avatud 10-21
-        </Popup>
-      </Marker>
-      <Marker position={[58.3778, 26.7298]}>
-        <Popup>
-          Tasku keskus. <br /> Avatud 10-21
-        </Popup>
-      </Marker>
-
-      {/*  */}
-    </MapContainer>
-  </div>)
+    <div>
+      <MapContainer className='map' center={defaultPosition} zoom={8} scrollWheelZoom={true}>
+        <ChangeView center={defaultPosition} zoom={8} />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {mapCoordinates.map(coord => (
+          <Marker key={coord.name} position={[coord.long, coord.lat]}>
+            <Popup>
+              {coord.name}. <br /> Avatud {coord.openTime}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
 }
 
-export default Map; 
+export default Map;
+
+
+
+
+// import L from 'leaflet';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import icon from 'leaflet/dist/images/marker-icon.png';
+// import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+// import ChangeView from './ChangeView';
+// import '../css/Map.css';
+
+// let DefaultIcon = L.icon({
+//   iconUrl: icon,
+//   shadowUrl: iconShadow,
+//   iconSize: [25,41], 
+//   iconAnchor: [12,41],
+//   popupAnchor: [2, -40],
+// });
+// L.Marker.prototype.options.icon = DefaultIcon;
+
+// function Map(props) { 
+
+//   return (
+//   <div>
+
+//     <MapContainer className='map' center={props.mapCoordinaates.lngLat} zoom={props.mapCoordinaates.zoom} scrollWheelZoom={true}>
+//       <ChangeView center={props.mapCoordinaates.lngLat} zoom={props.mapCoordinaates.zoom} />
+//       <TileLayer
+//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//       />
+//       <Marker position={[59.4231, 24.7991]}>
+//         <Popup>
+//           Ülemiste keskus. <br /> Avatud 9-20
+//         </Popup>
+//       </Marker>
+//       <Marker position={[59.4277, 24.7193]}>
+//         <Popup>
+//           Kristiine keskus. <br /> Avatud 10-21
+//         </Popup>
+//       </Marker>
+//       <Marker position={[58.3778, 26.7298]}>
+//         <Popup>
+//           Tasku keskus. <br /> Avatud 10-21
+//         </Popup>
+//       </Marker>
+
+//       {/*  */}
+//     </MapContainer>
+//   </div>)
+// }
+
+// export default Map; 
--- webshop/src/pages/global/Shops.jsx
+++ webshop/src/pages/global/Shops.jsx
@@ -1,21 +1,110 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import Map from '../../components/Map';
-import Map2 from '../../components/Map2';
 import Button from '@mui/material/Button';
+import { Spinner } from 'react-bootstrap';
 
 function Shops() {
-  const [coordinaates, setCoordinates] = useState({lngLat: [59.4378, 24.7574], zoom: 11});
+  const [coordinates, setCoordinates] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [shopNames, setShopNames] = useState([]);
+  const [filteredCoordinates, setFilteredCoordinates] = useState([]);
+  const url = process.env.REACT_APP_SHOPS_DB_URL;
 
-  return (<div className="map">
-    <br />
-    <Button variant="outlined" onClick={() => setCoordinates({lngLat: [59.4378, 24.7574], zoom: 11})}>Kõik poed</Button>{' '}
-    <Button variant="outlined" onClick={() => setCoordinates({lngLat: [59.4231, 24.7991], zoom: 13})}>Ülemiste</Button>{' '}
-    <Button variant="outlined" onClick={() => setCoordinates({lngLat: [59.4277, 24.7193], zoom: 13})}>Kristiine</Button>{' '}<br /><br />
-    <Map mapCoordinaates={coordinaates}  /><br />
-    {/* <div className='map2'>
-      <Map2 />
-    </div> */}
-  </div>)
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        setCoordinates(json);
+        setFilteredCoordinates(json); // Initialize with all shops
+        const uniqueShops = [...new Set(json.map(coord => coord.name))];
+        setShopNames(uniqueShops);
+        setLoading(false);
+      });
+  }, [url]);
+
+  const filterByShops = (shopName) => {
+    const result = coordinates.filter(coord => coord.name === shopName);
+    setFilteredCoordinates(result);
+  };
+
+  if (isLoading) {
+    return <Spinner animation="border" />;
+  }
+
+  return (
+    <div className="map">
+      <br />
+      {shopNames.map(shop => (
+        <Button key={shop} variant="contained" onClick={() => filterByShops(shop)}>
+          {shop}
+        </Button>
+      ))}
+      <br /><br />
+      <Map mapCoordinates={filteredCoordinates} /><br />
+    </div>
+  );
 }
 
-export default Shops
+export default Shops;
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react'
+// import Map from '../../components/Map';
+// // import Map2 from '../../components/Map2';
+// import Button from '@mui/material/Button';
+// import { useEffect } from 'react';
+// import {Spinner} from "react-bootstrap";
+
+// function Shops() {
+//   const [coordinates, setCoordinates] = useState([]);
+//   const [isLoading, setLoading] = useState(true);
+//   const [dbCoordinates, setDbCoordinates] = useState([]);
+//   const url = process.env.REACT_APP_SHOPS_DB_URL;
+
+//   useEffect(() => {
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(json => {
+//       setCoordinates(json);
+//       setDbCoordinates(json);
+//       setLoading(false);
+//       const result = [...new Set(json.map(coordinates => coordinates.name))];
+//       setDbCoordinates(result);
+//     });
+//   }, [url]);
+
+//   function filterByShops(coordinatesClicked) {
+//     // reset();
+//     const result = dbCoordinates.filter((p) => p.coordinates === coordinatesClicked);
+//     setCoordinates(result);
+//   }
+
+//   if (isLoading) {
+//     return <Spinner />
+//   }
+
+//   return (<div className="map">
+//     <br />
+//     {coordinates.map(category => 
+//       <Button key={coordinates.name} variant="contained" onClick={() => filterByShops(coordinates.name)}>
+//         {coordinates.name}
+//         </Button>
+//     )}
+//     {/* <Button variant="outlined" onClick={() => setCoordinates({lngLat: [59.4378, 24.7574], zoom: 11})}>All shops</Button>{' '}
+//     <Button variant="outlined" onClick={() => setCoordinates({lngLat: [59.4231, 24.7991], zoom: 13})}>Ülemiste</Button>{' '}
+//     <Button variant="outlined" onClick={() => setCoordinates({lngLat: [59.4277, 24.7193], zoom: 13})}>Kristiine</Button>{' '}<br /><br /> */}
+//     <Map mapCoordinaates={coordinates}  /><br />
+//     {/* <div className='map2'>
+//       <Map2 />
+//     </div> */}
+//   </div>)
+// }
+
+// export default Shops
