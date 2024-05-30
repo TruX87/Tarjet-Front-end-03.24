ed7bf8ffb2da230154f0064869f6b472f21b38af
TruX87 <163743905+TruX87@users.noreply.github.com>
2024-05-30 12:15:55 +0300

    webshop login-signup-auth

--- webshop/src/pages/auth/Signup.jsx
+++ webshop/src/pages/auth/Signup.jsx
@@ -1,53 +1,19 @@
-import React, { Component } from 'react'
-import '../../css/Login-Signup.css'
-
-export default class SignUp extends Component {
-  render() {
-    return (
-      <form className='signup-form'>
-        <h3>Sign Up</h3>
-
-        <div className="mb-3">
-          <label>First name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
-          />
-        </div>
-
-        <div className="mb-3">
-          <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" />
-        </div>
-
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
-
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
-
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Sign Up
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Already registered <a href="/login">sign in?</a>
-        </p>
-      </form>
-    )
-  }
+import React from "react";
+import AuthForm from "./AuthForm";
+ 
+function Signup() {
+  return (
+    <AuthForm  
+    headerText="Sign up"
+    isUsername={true}
+    isConfirmPassword={true}
+    footerText="Already have an account?"
+    linkUrl="/login"
+    linkText="Login"
+    isRememberMe={false}
+    url={"signUp"}
+    />
+  );
 }
+ 
+export default Signup;
