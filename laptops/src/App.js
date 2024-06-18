447d9f9f9b2c5ff59d9e9d49a77b9a2294659a8b
TruX87 <163743905+TruX87@users.noreply.github.com>
2024-06-18 11:22:53 +0300

    laptops-ostukorv-avaleht-kuva

--- laptops/src/App.js
+++ laptops/src/App.js
@@ -4,6 +4,7 @@ import Avaleht from './pages/Avaleht';
 import VaataArvuteid from './pages/VaataArvuteid';
 import LisaArvuti from './pages/LisaArvuti';
 import Tooted from './pages/Tooted';
+import Ostukorv from './pages/Ostukorv';
 
 function App() {
   return (
@@ -21,12 +22,16 @@ function App() {
       <Link to="/products">
         <button>Vaata tooteid</button>
       </Link>
+      <Link to="/cart">
+        <button>Ostukorv</button>
+      </Link>
       <br /><br />
       <Routes>
         <Route path="" exact element={<Avaleht />} />
         <Route path="all" exact element={<VaataArvuteid />} />
         <Route path="add" exact element={<LisaArvuti />} />
         <Route path="products" exact element={<Tooted />} />
+        <Route path="cart" exact element={<Ostukorv />} />
       </Routes>
     </div>
   );
